095da589f0c64e77cdc6fedddcfc4e1524af0db2
PTH\pth <pth6310@naver.com>
2023-07-27 19:34:48 +0900

    testtt

--- src/main/java/com/pth/jwt/JwtApplication.java
+++ src/main/java/com/pth/jwt/JwtApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * test2
+ * test23
  */
 @SpringBootApplication
 public class JwtApplication {
